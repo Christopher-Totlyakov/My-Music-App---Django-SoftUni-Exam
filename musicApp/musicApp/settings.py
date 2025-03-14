4526bef99122d99d7b390e6d53bf34e50a4522e8
Christopher Totlyakov <laminex0622@gmail.com>
2025-03-14 14:53:49 +0200

    add Profile DB model

--- musicApp/musicApp/settings.py
+++ musicApp/musicApp/settings.py
@@ -80,9 +80,13 @@ WSGI_APPLICATION = 'musicApp.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "musicapp",
+        "USER": "admin",
+        "PASSWORD": "secret",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
---
+++ musicApp/profiles/validators.py
@@ -0,0 +1,21 @@
+from django.utils.deconstruct import deconstructible
+
+@deconstructible
+class AlphaNumericValidator:
+    def __init__(self, message=None):
+        self.message = message
+
+    @property
+    def message(self):
+        return self.__message 
+    
+    @message.setter
+    def message(self, value):
+        if value is None:
+            self.__message = "Ensure this value contains only letters, numbers, and underscore."
+        else:
+            self.__message = value
+
+    def __call__(self, value: str, *args, **kwargs):
+        if value != slugify(value):
+            return ValidationError(self.message)
