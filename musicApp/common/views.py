9115ac0ab3ad1e138bae62b4dfabe97a6f7b1367
Christopher Totlyakov <laminex0622@gmail.com>
2025-03-14 23:47:21 +0200

    add common app and home page

---
+++ musicApp/common/views.py
@@ -0,0 +1,20 @@
+from django.urls import reverse_lazy
+from django.views.generic import ListView
+from django.views.generic.edit import BaseFormView
+from musicApp.utils import get_user_obj
+from albums.models import Album
+from profiles.forms import ProfileCreateForm
+
+
+class HomePage(ListView, BaseFormView):
+    model = Album
+    form_class = ProfileCreateForm
+    success_url = reverse_lazy('home')
+
+    def get_template_names(self):
+        prfile = get_user_obj()
+
+        if prfile:
+            return ['profiles/home-with-profile.html']
+
+        return ['profiles/home-no-profile.html']
--- musicApp/musicApp/settings.py
+++ musicApp/musicApp/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'musicApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,9 +127,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILE_DIRS = (
+STATICFILES_DIRS = [
     BASE_DIR / 'static',
-)
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
