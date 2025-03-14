c9870a118f5712a1bbbdf1543608bb89324480ec
Christopher Totlyakov <laminex0622@gmail.com>
2025-03-14 15:29:01 +0200

    add albums DB model

--- musicApp/albums/models.py
+++ musicApp/albums/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from albums.choices import GenreChoices
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class Album(models.Model):
+    album_name = models.CharField(
+        max_length = 30,
+        unique = True,
+        )
+
+    artist = models.CharField(
+        max_length = 30,
+        )
+    
+    genre = models.CharField(
+        max_length = 50,
+        choices=GenreChoices.choices,
+        )
+
+    description = models.TextField(
+        blank = True,
+        null = True,
+    )
+
+    image_url = models.URLField()
+
+    price = models.FloatField(
+        validators=[
+            MinValueValidator(0.0),
+        ]
+    )
+
+    owner = models.ForeignKey(
+        to = 'profiles.Profile',
+        related_name = "albums",
+        on_delete = models.CASCADE,
+    )
